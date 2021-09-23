9f64e9c1f200bc8b876038b4740047f54c27a560
damarvijis <damar.j@mail.ugm.ac.id>
2021-09-24 01:50:20 +0700

    damarvijis3

--- routes/product.js
+++ routes/product.js
@@ -3,10 +3,10 @@ const route = express.Router();
 const Controller = require("../controllers/controller");
 
 route.get("/:id", Controller.productList); // product page (habis login)
+route.get("/:UserId/add"); // dari button add product
+route.post("/:UserId/add");
 route.get("/:UserId/edit/:id"); // dari button edit product
 route.post("/:UserId/edit/:id");
-route.get("/:UserId/add/:id"); // dari button add product
-route.post("/:UserId/add/:id");
 route.get("/:UserId/delete/:id"); // dari button delete product by id
 
 
